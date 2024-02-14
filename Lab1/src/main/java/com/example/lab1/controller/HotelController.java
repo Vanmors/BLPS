8db0318353fc577bf6c18bc5ee55f751f2d4552c
ivanmorikov <ivanmorikov48@gmail.com>
2024-02-14 16:12:18 +0300

    Reservation request has been added

--- Lab1/src/main/java/com/example/lab1/controller/HotelController.java
+++ Lab1/src/main/java/com/example/lab1/controller/HotelController.java
@@ -1,14 +1,14 @@
 package com.example.lab1.controller;
 
+import com.example.lab1.dto.ReservationDTO;
 import com.example.lab1.entity.HotelNumber;
+import com.example.lab1.entity.Reservation;
 import com.example.lab1.service.HotelNumberService;
+import com.example.lab1.service.ReservationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,15 +19,21 @@ import java.util.List;
 public class HotelController {
 
     private final HotelNumberService hotelService;
+    private final ReservationService reservationService;
 
     @GetMapping()
     public ResponseEntity<List<HotelNumber>> getHotels(@RequestParam LocalDate dataBegin,
                                                        @RequestParam LocalDate dataEnd,
                                                        @RequestParam String city) {
         var hotels = hotelService.getAvailableRooms(city, dataBegin, dataEnd);
-        if (hotels == null || hotels.size() == 0){
+        if (hotels == null || hotels.size() == 0) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(hotelService.getAvailableRooms(city, dataBegin, dataEnd), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Reservation> chooseHotelRoom(@RequestBody ReservationDTO reservationDTO) {
+        return new ResponseEntity<>(reservationService.create(reservationDTO), HttpStatus.OK);
     }
 }
