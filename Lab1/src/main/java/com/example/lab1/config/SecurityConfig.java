61c72cc72168254afde1886e7653a0d120bb28a1
ivanmorikov <ivanmorikov48@gmail.com>
2024-03-10 21:30:22 +0300

    Security config has been updated

--- Lab1/src/main/java/com/example/lab1/config/SecurityConfig.java
+++ Lab1/src/main/java/com/example/lab1/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.lab1.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +13,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .requestMatchers(HttpMethod.GET, "/hotel").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/hotel").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/hotel").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/hotel").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
